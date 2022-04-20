14f8a19294328d0f4a66f201cc5eaa1cceb8c6ff
LazizbekShodmonov <lazizbekshodmonov001@gmail.com>
2022-04-20 21:45:53 +0500

    install firebase

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-analytics.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCM1hlPITZmsK3mcZfDIgXLZkqSzPT4EzY",
+//   authDomain: "cozy-house-a5937.firebaseapp.com",
+//   projectId: "cozy-house-a5937",
+//   storageBucket: "cozy-house-a5937.appspot.com",
+//   messagingSenderId: "1064649545894",
+//   appId: "1:1064649545894:web:fcf98f247d99c420307c67",
+//   measurementId: "G-YX4X50GMFQ"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// console.log(analytics);
+// let url = './data/data.json';
+
+// fetch(url)
+//       .then(data => data.json())
+//       .then(data => {
+//             console.log(data);
+//       });
+
+
