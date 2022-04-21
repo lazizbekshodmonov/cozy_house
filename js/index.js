0c049c84f5d64201e0d273a24cecaaac8c8933d9
Lazizbek Shodmonov <lazizbekshodmonov001@gmail.com>
2022-04-21 22:31:22 +0500

    firebase delete

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCM1hlPITZmsK3mcZfDIgXLZkqSzPT4EzY",
-  authDomain: "cozy-house-a5937.firebaseapp.com",
-  projectId: "cozy-house-a5937",
-  storageBucket: "cozy-house-a5937.appspot.com",
-  messagingSenderId: "1064649545894",
-  appId: "1:1064649545894:web:fcf98f247d99c420307c67",
-  measurementId: "G-YX4X50GMFQ"
+  apiKey: "AIzaSyA2JY0d5L84gtGCsO4JJaRTT1noUMMjmbY",
+  authDomain: "cozy-house-9d154.firebaseapp.com",
+  databaseURL: "https://cozy-house-9d154-default-rtdb.firebaseio.com",
+  projectId: "cozy-house-9d154",
+  storageBucket: "cozy-house-9d154.appspot.com",
+  messagingSenderId: "942843098137",
+  appId: "1:942843098137:web:14c308ae10c3ba6bb66688",
+  measurementId: "G-38QBHMD4QV"
 };
 
 // Initialize Firebase
@@ -21,6 +22,3 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 console.log(analytics);
-
-
-
